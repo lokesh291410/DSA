30c12be9fa6c2dfc6c41fa13846e3f4e0cf333c3
Lokesh Kumar <157511893+lokesh291410@users.noreply.github.com>
2025-09-18 01:51:34 +0530

    Time: 25 ms (27.78%), Space: 51 MB (43.13%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -1,19 +1,24 @@
 class Solution {
-    private int rec(int i,int j,String s1,String s2,int[][] dp){
-        if(i<0 || j<0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        if(s1.charAt(i) == s2.charAt(j))
-        return dp[i][j] = 1 + rec(i-1,j-1,s1,s2,dp);
-
-        return dp[i][j] = Math.max(rec(i-1,j,s1,s2,dp),rec(i,j-1,s1,s2,dp));
-    }
     public int longestCommonSubsequence(String text1, String text2) {
-        int m = text1.length();
-        int n = text2.length();
-        int[][] dp = new int[m][n];
-        for(int[] row:dp) Arrays.fill(row,-1);
+        int[][] dp = new int[text1.length()][text2.length()];
+        for(int i=0; i<dp.length; i++) Arrays.fill(dp[i], -1);
 
-        return rec(m-1,n-1,text1,text2,dp);
+        return find(text1.length()-1, text2.length()-1, text1, text2, dp);
+    }
+
+    public int find(int idx1, int idx2, String s1, String s2, int[][] dp) {
+        if(idx1 < 0 || idx2 < 0) return 0;
+
+        if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
+
+        if(s1.charAt(idx1) == s2.charAt(idx2))
+            return 1 + find(idx1 - 1, idx2 - 1, s1, s2, dp);
+        
+        dp[idx1][idx2] = Math.max(
+                            find(idx1 - 1, idx2, s1, s2, dp),
+                            find(idx1, idx2 - 1, s1, s2, dp)
+                        );
+
+        return dp[idx1][idx2];
     }
 }
